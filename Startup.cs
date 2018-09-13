4068e6db48a485b6c1bab6bc64cc87d0c8341a9f
Nicholas Brooks <nicholas.brooks@infocentral.com.au>
2018-09-13 18:24:12 +1000

    Upgraded to angular 6 and typescript 2.7.2

--- Startup.cs
+++ Startup.cs
@@ -62,7 +62,7 @@ namespace nswagnetfull
 
                 if (env.IsDevelopment())
                 {
-                    spa.UseAngularCliServer(npmScript: "start");
+                    spa.UseProxyToSpaDevelopmentServer("http://localhost:4200");
                 }
             });
         }
